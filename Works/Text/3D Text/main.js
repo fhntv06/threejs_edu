0c1ff89823c3c7447476dce144fa30880c049680
kartemake <a.kuskov@make.st>
2022-08-30 20:09:17 +0700

    попытка работы с текстом

---
+++ Works/Text/3D Text/main.js
@@ -0,0 +1,25 @@
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
+camera.position.set( 0, 0, 100 );
+camera.lookAt( 0, 0, 0 );
+
+const scene = new THREE.Scene();
+const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+const points = [];
+
+points.push( new THREE.Vector3( -45, 20, 0 ) );
+points.push( new THREE.Vector3( -20, 20, 0 ) );
+points.push( new THREE.Vector3( -20, 30, 0 ) );
+points.push( new THREE.Vector3( 0, 15, 0 ) );
+points.push( new THREE.Vector3( -20, 0, 0 ) );
+points.push( new THREE.Vector3( -20, 10, 0 ) );
+points.push( new THREE.Vector3( -45, 10, 0 ) );
+points.push( new THREE.Vector3( -45, 20, 0 ) );
+
+const geometry = new THREE.BufferGeometry().setFromPoints( points );
+const line = new THREE.Line( geometry, material );
+scene.add( line );
+renderer.render( scene, camera );
