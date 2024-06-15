48f91d2615a3f008510e1313386513f4903a77ee
kartem <k6art5@gmail.com>
2024-06-15 17:10:17 +0700

    change logic to get id product brandbottle

--- Works/brandbottle/src/components/DetailView/DetailView.js
+++ Works/brandbottle/src/components/DetailView/DetailView.js
@@ -1,16 +1,19 @@
 import React from "react";
+import { useParams } from 'react-router-dom';
 
 import './DetailView.css';
 
 import Scene from '../../threeJS/model/Scene';
-export default function DetailView ({ numberProduct }) {
+export default function DetailView () {
+    const { id } = useParams();
+
     return (
         <div className="container__detail_text">
             <div className="detail__title">
-                <h1>Трансформатор 110кВ</h1>
+                <h1>Трансформатор 110кВ №{id}</h1>
                 <h4>Описание</h4>
             </div>
-            <Scene />
+            {/* <Scene /> */}
             <div className="detail__description">
                 <p>Параметр 1</p>
                 <p>Параметр 2</p>
--- Works/brandbottle/src/components/Products/Products.js
+++ Works/brandbottle/src/components/Products/Products.js
@@ -1,32 +1,28 @@
 import React from "react";
 import { NavLink } from 'react-router-dom';
-import DetailView from '../DetailView/DetailView';
 
 import './Products.css';
 
-export default function Products ({ numberProduct }) {
+export default function Products () {
     return (
-        numberProduct ? <DetailView numberProduct={numberProduct} /> 
-        : (
-            <div className="container__products">
-                <h1>Продукция</h1>
-                <div className="products__items">
-                    <NavLink to={'/products/1'}>
-                        <div className="item">
-                            <h3>Трансформатор 110кВ</h3>
-                            <div className="image-anons"></div>
-                            <div className="date">20.08.2035</div>
-                        </div>
-                    </NavLink>
-                    <NavLink to={'/products/2'}>
-                        <div className="item">
-                            <h3>Трансформатор 220кВ</h3>
-                            <div className="image-anons"></div>
-                            <div className="date">20.08.2025</div>
-                        </div>
-                    </NavLink>
-                </div>
-            </div>)
-        
+        <div className="container__products">
+            <h1>Продукция</h1>
+            <div className="products__items">
+                <NavLink to={'/products/1'}>
+                    <div className="item">
+                        <h3>Трансформатор 110кВ</h3>
+                        <div className="image-anons"></div>
+                        <div className="date">20.08.2035</div>
+                    </div>
+                </NavLink>
+                <NavLink to={'/products/2'}>
+                    <div className="item">
+                        <h3>Трансформатор 220кВ</h3>
+                        <div className="image-anons"></div>
+                        <div className="date">20.08.2025</div>
+                    </div>
+                </NavLink>
+            </div>
+        </div>
     )
 }
